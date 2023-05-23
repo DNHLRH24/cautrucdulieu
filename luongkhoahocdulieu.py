b81bc8cb83ecdab83f1310e120088733eec5e145
Quocdank3 <115203375+Quocdank3@users.noreply.github.com>
2023-05-23 22:31:49 +0700

    Add files via upload

--- luongkhoahocdulieu.py
+++ luongkhoahocdulieu.py
@@ -74,6 +74,13 @@ def bieu_do_remote_ratio():
    plt.ylabel('Mật độ')
    plt.title('Phân phối tỷ lệ từ xa')
    plt.show()
+
+def cap_do_kinh_nghiem():
+   fig, ax = plt.subplots()
+   sns.countplot(ax = ax, data = data, x = data.experience_level)
+   ax.set(xlabel='', ylabel='Counts', title='Cấp độ kinh nghiệm')
+   ax.bar_label(ax.containers[0])
+   plt.show()
 def main():
     bieu_do_tron_experience_level()
     bieu_do_cot_median()
@@ -81,19 +88,61 @@ def main():
     alltime_top_ten()
     alltime_top_ten_2023()
     experience_level_vs_work_year()
-    bieu_do_remote_ratio()
-#if __name__=="__main__":
+   #  bieu_do_remote_ratio()
+    cap_do_kinh_nghiem()
+if __name__=="__main__":
     main()
 
-# Create histogram
+# Đặt màu nền và kích thước hình
+plt.style.use('dark_background')
+plt.figure(figsize = (10, 6))
+# tạo biểu đồ
 sns.histplot(x = 'salary_in_usd', hue = 'experience_level', multiple = 'stack',
              edgecolor = '#cfd0d4', bins = 50, data = data, palette = 'viridis')
 
-# Customize the title and labels
+# Tùy chỉnh tiêu đề và nhãn
 plt.grid(alpha = 0.3)
-plt.title('Distribution of Salary (USD) by Experience')
+plt.title('Phân phối tiền lương (USD) theo kinh nghiệm')
 plt.xlabel('Salary')
 plt.ylabel('Count')
-
-# Display the plot
 plt.show()
+#lọc dữ liệu
+chuong=data.loc[data['job_title']=='Data Scientist']
+chuong1=data.loc[data['job_title']=='ML Engineer']
+chuong2=data.loc[data['job_title']=='Applied Scientist']
+#tính tỷ lệ phần trăm cho mỗi nhóm
+sizes=[len(chuong),len(chuong1),len(chuong2)]
+# biểu đồ hình tròn phần trăm công việc
+my_explode = (0.1,0,0)
+my_colors = ['red','orange','silver',]
+plt.pie(sizes,labels=['Data Scientist','ML Engineer','Applied Scientist'], explode=my_explode, colors=my_colors, autopct='%.1f%%')
+plt.title('Phân bố các vị trí công việc')
+plt.show()
+
+# tạo bản thông kê
+dieu_kien1=['M','L','S']
+quy_mo='company_size'
+tan_so1=data[quy_mo].value_counts()
+print(data)
+
+#biểu đồ quy mô các công ty
+my_colors = ['lightblue','lightsteelblue','silver']
+my_explode = (0, 0.1, 0)
+plt.pie(tan_so1,labels=dieu_kien1,autopct="%.1f%%",startangle=15,shadow=True,colors=my_colors,explode=my_explode)
+plt.title("phân bố quy mô công ty")
+plt.axis('equal')
+plt.show()
+
+import country_converter as coco
+import pandas as pd
+import plotly.express as px
+rr = data.groupby('company_location')['remote_ratio'].mean().reset_index()
+rr['company_location'] =  coco.convert(names = rr['company_location'], to = "ISO3")
+rr.head()
+fig = px.choropleth(rr,
+                    locations = rr.company_location, 
+                    color = rr.remote_ratio,                  
+                    labels={'company_location':'Quốc gia','remote_ratio':'Tỷ lệ công việc từ xa'})
+                    
+fig.update_layout(title = "Vị trí công việc từ xa")
+fig.show()
